c5fe10d2fd387dcd1a024dd906040f74d39fcb0d
Surender Pratap Singh <86663308+Surender0905@users.noreply.github.com>
2023-10-16 12:36:31 +0530

    Update middleware.ts
    change in middle ware

--- src/middleware.ts
+++ src/middleware.ts
@@ -194,7 +194,7 @@ export function middleware(request: NextRequest) {
 //   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 // };
 
-export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-};
+// export const config = {
+//   // Matcher ignoring `/_next/` and `/api/`
+//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+// };
